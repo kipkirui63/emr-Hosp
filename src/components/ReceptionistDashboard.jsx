76b4f07fed9847949b24ea6dd81fbea4aa6c4312
kipkirui63 <enocksang8356@gmail.com>
2024-02-26 00:12:15 +0300

    added aplcae used by receptionist

---
+++ src/components/ReceptionistDashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PatientRegistrationForm from "./PatientRegistrationForm";
+
+function ReceptionistDashboard() {
+  return (
+    <div className="container">
+      <h2>Receptionist Dashboard</h2>
+      <PatientRegistrationForm />
+    </div>
+  );
+}
+
+export default ReceptionistDashboard;
